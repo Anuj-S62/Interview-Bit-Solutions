bd4d7015282ee4d1c6ed8d92792207087138c0d2
Anuj Solanki <anuj603362@gmail.com>
2022-01-19 21:28:43 +0530

    added

--- palindrome_list.cpp
+++ palindrome_list.cpp
@@ -17,15 +17,12 @@ int lPalin(ListNode* A) {
         ptr=ptr->next;
     }
     cout<<endl;
-    for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<" ";
-    }
     for(int i=0;i<arr.size()/2;i++){
         if(arr[i]!=arr[arr.size()-1-i]){
             return 0;
         }
     }
-    return -1;
+    return 1;
 }
 
 ListNode *show(struct ListNode *top)
