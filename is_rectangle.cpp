515b1d80c1128552517f70db18370f5157268f7e
Anuj Solanki <anuj603362@gmail.com>
2022-01-27 20:05:34 +0530

    added

---
+++ is_rectangle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int A, int B, int C, int D) {
+    if(A==B && C==D){
+        return 1;
+    }
+    else if(A==C && B==D){
+        return 1;
+    }
+    else if(A==C && B==D){
+        return 1;
+    }
+    else if(A==C && B==D && A==B){
+        return 1;
+    }
+    return 0;
+}
+
+
+int main(){
+    
+
+    return 0;
+}
