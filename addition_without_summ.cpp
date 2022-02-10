56e883e92137ea3ee1b6d3b6717010645d2a3f6a
Anuj Solanki <anuj603362@gmail.com>
2022-02-10 18:50:56 +0530

    added

---
+++ addition_without_summ.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int addNumbers(int A, int B) {
+    for(int i=0;i<A;i++){
+        B++;
+    }
+    return B;
+}
+
+
+int main(){
+    
+
+    return 0;
+}
