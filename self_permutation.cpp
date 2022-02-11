4e68234db0005031e1e401fea8cb8d94ef154751
Anuj Solanki <anuj603362@gmail.com>
2022-02-11 20:39:35 +0530

    added

---
+++ self_permutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int permuteStrings(string A, string B) {
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    if(A==B) return 1;
+    return 0;
+}
+
+int main(){
+    
+
+    return 0;
+}
