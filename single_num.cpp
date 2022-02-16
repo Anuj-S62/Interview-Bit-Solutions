f5a471788f7dae7d19ece9ccf4b115ea9b964e2e
Anuj Solanki <anuj603362@gmail.com>
2022-02-16 18:18:15 +0530

    added

---
+++ single_num.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int singleNumber(const vector<int> &A) {
+       int n = A.size();
+       int result = 0;
+       for (int i = 0; i < n; i++) {
+           result ^= A[i];
+           cout<<result<<endl;
+       }
+       return result;
+    }
+
+int main(){
+    vector<int> arr={1,2,2,1,3};
+    cout<<singleNumber(arr)<<endl;
+
+    return 0;
+}
