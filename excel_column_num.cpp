953ffc4b07d8b586a65ab2e0278a2679a27d2e11
Anuj Solanki <anuj603362@gmail.com>
2022-02-20 17:16:40 +0530

    added

---
+++ excel_column_num.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int titleToNumber(string s) {
+       int result = 0;
+       for (int i = 0; i < s.size(); i++) {
+               result = result * 26 + (s[i] - 'A' + 1);
+       }
+       return result;
+    }
+
+
+
+int main(){
+    cout<<titleToNumber("AABB")<<endl;
+
+    return 0;
+}
