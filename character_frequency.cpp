9851d280ed3fbab0dd37e1d3d4780d01c5d424b3
Anuj Solanki <anuj603362@gmail.com>
2022-02-22 19:51:07 +0530

    added

---
+++ character_frequency.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void show(vector<int> arr){
+    // cout<<arr.size();
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+vector<int> solve(string A) {
+    vector<int> temp;
+    for(int i=0;i<26;i++){
+        temp.push_back(0);
+    }
+    for(int i=0;i<A.length();i++){
+        int idx=A[i]-97;
+        temp[idx]++;
+    }
+    vector<int> freq;
+    // for(int i=0;i<26;i++){
+    //     if(temp[i]) freq.push_back(temp[i]);
+    // }
+    // show(freq);
+    // temp.clear();
+    // temp.push_back(3);
+    show(temp);
+
+    for(int i=0;i<A.length();i++){
+        int idx=A[i]-97;
+        if(temp[idx]) freq.push_back(temp[idx]);
+        temp[idx]=0;
+    }
+    show(freq);
+    return temp;
+}
+
+int main(){
+    solve("interviewbit");
+
+    return 0;
+}
