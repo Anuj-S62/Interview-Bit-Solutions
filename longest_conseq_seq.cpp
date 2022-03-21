be539e7e77febeacf6db35f112e53d76822afce6
Anuj Solanki <anuj603362@gmail.com>
2022-03-21 20:24:53 +0530

    added level 6

--- longest_conseq_seq.cpp
+++ longest_conseq_seq.cpp
@@ -1,33 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// int longestConsecutive(const vector<int> &A) {
+//     vector<int> a = A;
+//     sort(a.begin(),a.end());
+//     int ans = 0;
+//     int temp = 1;
+//     for(int i=1;i<a.size();i++){
+//         if(a[i]==a[i-1])continue;
+//         if(a[i] == a[i-1] + 1) temp++;
+//         else temp = 1;
+//         ans = max(ans,temp);
+//     }
+//     ans = max(ans,temp);
+//     return ans;
+// }
+
 int longestConsecutive(const vector<int> &A) {
     unordered_set<int> st;
     for(int i=0;i<A.size();i++){
         st.insert(A[i]);
     }
-    int ans = 0;
+    int length = 0;
     int temp = 1;
-    ans = max(ans,temp);
     int x;
-    unordered_set<int> ::iterator itr = st.begin();
-    while(itr!=st.end()){
-        // cout<<temp<<endl;
-        x = *st.begin();
-        if((st.find(x+1)!=st.end()) || (st.find(x-1)!=st.end())){
+    for(int i=0;i<A.size();i++){
+        x = A[i] + 1;
+        while(st.find(x)!=st.end()){
+            st.erase(x);
             temp++;
-            ans = max(ans,temp);
+            x = x + 1;
         }
-        else{
-            temp = 1;
+        x = A[i] - 1;
+        while(st.find(x)!=st.end()){
+            st.erase(x);
+            temp++;
+            x = x-1;
         }
-        itr++;
+        length = max(length,temp);
+        temp = 1;
     }
-    return ans;
+    return length;
 }
 
 int main(){
-    vector<int> arr = {100,4,200,1,3,2};
+    vector<int> arr = {100,4,200,2};
     cout<<longestConsecutive(arr)<<endl;
 
     return 0;
